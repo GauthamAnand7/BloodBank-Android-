ed7d6eb5d10d26b5476ebc3a71f7e5ef5b7be52e
GauthamAnand7 <gautham1327@gmail.com>
2020-03-30 01:07:30 +0530

    Final Commit

---
+++ BloodBank/app/src/main/java/com/ideabox/bloodbank/DonerData.java
@@ -0,0 +1,13 @@
+package com.ideabox.bloodbank;
+public class DonerData {
+
+    public int id;
+    public String full_name;
+    public String email;
+    public String phone;
+    public String addr;
+    public String bloodgrp;
+    public String city;
+    public String area;
+
+}
